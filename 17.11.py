4af9d7fc4b9c8d4436f59a234361702b3731255c
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-11-17 16:24:37 +0300

    Add files via upload

---
+++ 17.11.py
@@ -0,0 +1,16 @@
+# mevalar = ["olma","banana","apelsin","uzum"]
+#
+# for meva in mevalar:
+#     print(f"Men {meva} yaxshi koraman")
+
+
+# dostlar = ["Xikmatjon","Husniddin","Maqsadbek","Davlatyor"]
+#
+# for dost in dostlar:
+#     if dost == 'Xikmatjon':
+#         print(f"{dost} - Salom jigar")
+#     else:
+#         print(f"Salom {dost}")
+
+
+
